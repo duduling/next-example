d63db2854b7e66ba8aee579481c326a1563c8f8c
Duduling <duduling20@gmail.com>
2020-06-16 18:05:56 +0900

    feat: Example for navigation

---
+++ pages/profile.js
@@ -0,0 +1,12 @@
+import Header from '../components/Header'
+
+function Profile() {
+    return (
+        <div>
+            <Header />
+            <p>Hello, my name is DuHyeon Kim. I use next.js</p>
+        </div>
+    )
+}
+
+export default Profile
